1921f031ac70e262640b8d78c6fdda5362250c65
K-Wei <kevinwei@users.noreply.github.com>
2017-02-26 02:17:07 -0600

    created rcav for user index and show page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+    render("users/index.html.erb")
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @photos = Photo.where(:user_id => @user.id)
+    @likes = Like.where(:user_id => @user.id)
+    @comments = Comment.where(:user_id => @user.id)
+
+    render("users/show.html.erb")
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -52,5 +52,11 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # READ
+  # practice using shortened route
+  get "/users" => "users#index"
+  get "/users/:id" => "users#show"
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
